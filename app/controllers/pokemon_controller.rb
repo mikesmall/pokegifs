de1f1734cdbcf6b86867b590294a8007f838f0f5
mikesmall <michael.l.a.small@gmail.com>
2017-06-28 17:39:47 -0400

    Name output works at localhost:3000/pokemon/1

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,16 +2,24 @@
 
 class PokemonController < ActionController::Base
 
-  # Pasted from assignment:
 
-  res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
-  body = JSON.parse(res.body)
-  body["name"] # should be "pikachu"
+  def show
 
-  # Pasted from assignment:
+    # Pasted from assignment:
 
-  Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=pikachu&rating=g", followlocation: true)
+    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
+    search_results = JSON.parse(res.body)
+    # body["name"] # should be "pikachu"
 
-  #---------------------- Giphy key is saved in my .bash_profile as GIPHY_POKEGIFS_KEY
+    # Pasted from assignment:
+
+    Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=pikachu&rating=g", followlocation: true)
+    #---------------------- Giphy key is saved in my .bash_profile as GIPHY_POKEGIFS_KEY
+
+    render json: {
+      'name' => search_results["name"],
+      'types' => search_results["types"]
+    }
+  end
 
 end

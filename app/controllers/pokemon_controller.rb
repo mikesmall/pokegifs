327ba5401674201d086a3577fd068a911c4cd216
mikesmall <michael.l.a.small@gmail.com>
2017-06-28 18:15:17 -0400

    All done

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -7,21 +7,22 @@ class PokemonController < ActionController::Base
 
     # Pasted from assignment:
 
-    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
-    search_results = JSON.parse(res.body)
+    find_pokemons = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
+    pokemon_search_results = JSON.parse(find_pokemons.body)
     # body["name"] # should be "pikachu"
 
     # Pasted from assignment:
 
-    Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=#{params[:id]}&rating=g", followlocation: true)
+    find_gifs = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=#{params[:id]}&rating=g", followlocation: true)
     #---------------------- Giphy key is saved in my .bash_profile as GIPHY_POKEGIFS_KEY
+    gif_search_results = JSON.parse(find_gifs.body)
+
 
     render json: {
-      'name' => search_results["name"],
-      'types' => search_results["types"].first["type"]["name"]
+      'name' => pokemon_search_results["name"],
+      'types' => pokemon_search_results["types"].first["type"]["name"],
+      'gifs' => gif_search_results["data"].first["bitly_gif_url"]
     }
-
-
 
   end#def
 end#class

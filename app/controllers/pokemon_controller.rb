42a59d56d460ee78a225cc457fdb855c0ba23bf1
mikesmall <michael.l.a.small@gmail.com>
2017-06-28 17:44:02 -0400

    Added string interpolation

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -7,19 +7,21 @@ class PokemonController < ActionController::Base
 
     # Pasted from assignment:
 
-    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
+    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
     search_results = JSON.parse(res.body)
     # body["name"] # should be "pikachu"
 
     # Pasted from assignment:
 
-    Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=pikachu&rating=g", followlocation: true)
+    Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=#{params[:id]}&rating=g", followlocation: true)
     #---------------------- Giphy key is saved in my .bash_profile as GIPHY_POKEGIFS_KEY
 
     render json: {
       'name' => search_results["name"],
       'types' => search_results["types"]
     }
-  end
 
-end
+
+
+  end#def
+end#class

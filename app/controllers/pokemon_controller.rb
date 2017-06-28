3031c72100a7358b0305bc7cd4dacec04b6aebc1
mikesmall <michael.l.a.small@gmail.com>
2017-06-28 16:51:48 -0400

    Pasted code from 'Adding the Pokemon info' step

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,6 +2,10 @@
 
 class PokemonController < ActionController::Base
 
+  # Pasted from assignment:
   
+  res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
+  body = JSON.parse(res.body)
+  body["name"] # should be "pikachu"
 
 end

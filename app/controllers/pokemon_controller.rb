5e9dabc34c95102bcdaf3c285615cded1edc5819
mikesmall <michael.l.a.small@gmail.com>
2017-06-28 16:58:25 -0400

    Corrected syntax for using Giphy key

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,9 +3,15 @@
 class PokemonController < ActionController::Base
 
   # Pasted from assignment:
-  
+
   res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true)
   body = JSON.parse(res.body)
   body["name"] # should be "pikachu"
+
+  # Pasted from assignment:
+
+  Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_POKEGIFS_KEY"]}&q=pikachu&rating=g", followlocation: true)
+
+  #---------------------- Giphy key is saved in my .bash_profile as GIPHY_POKEGIFS_KEY
 
 end
